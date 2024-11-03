22cabc479e0ec62d90a371495b03407782c52fb1
Jhonathan <juridicaenlineaco@gmail.com>
2024-11-03 18:50:23 -0500

    Correccion del ehericio

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,7 +6,5 @@ length = parseFloat(document.getElementById('length').value);
 width = parseFloat(document.getElementById('width').value);
 
 let area = length * width;
+document.getElementById('result').innerText = `EL area del rectangulo is: ${area}`;
 }
-
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
-
